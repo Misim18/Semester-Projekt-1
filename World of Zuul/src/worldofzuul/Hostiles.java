6e705947d9e9fe1605ba8e1bdf548b5da042c5ad
Michael-Laptop <Michael-Laptop@DESKTOP-01AS2DF>
2019-11-05 12:42:53 +0100

    Went through Hostiles Class

--- World of Zuul/src/worldofzuul/Hostiles.java
+++ World of Zuul/src/worldofzuul/Hostiles.java
@@ -3,16 +3,16 @@ package worldofzuul;
 public class Hostiles extends Coordinate {
 private int Direction;
 private int Damage;
-private int startPostitionX;
-private int startPostitionY;
+//private int startPositionX;
+//private int startPositionY;
 
 public Hostiles(int dmg, int lvlsizeX, int lvlsizeY)
     {
-    super(0,0); //Changed later
+    super(0,0); //This seems to be nessesary as super wouldn't take a method returning an int, as an argument
+    this.setCoordinateX(setStartPositionX()); //sets the coordinates to the returned value of setStartPositionX (method)
+    this.setCoordinateY(setStartPositionY()); //sets the coordinates to the returned value of setStartPositionY (method)
     this.Damage = dmg;
     setDirection();
-    setStartPostitionX(lvlsizeX);
-    setStartPostitionY(lvlsizeY);
     
     }
 
@@ -21,12 +21,12 @@ public Hostiles(int dmg, int lvlsizeX, int lvlsizeY)
     }
 
     public void setDirection() {
-        int temp = (int) Math.round(Math.random());
-    
-        if(temp == 1){
-        this.Direction = 1;
-        } else {
+        if (this.getCoordinateX() == 0) {
+            this.Direction = 1;
+        } else if (this.getCoordinateX() == Game.getLimitX()) {
             this.Direction = -1;
+        } else {
+            System.out.println("Something went wrong with adding the direction");
         }
     }
 
@@ -38,15 +38,19 @@ public Hostiles(int dmg, int lvlsizeX, int lvlsizeY)
         this.Damage = Damage;
     }
 
-    public void setStartPostitionX(int lvlSizeX) {
-        int temp = (int)Math.round(Math.random()*lvlSizeX);
-        if(temp < lvlSizeX/2){this.startPostitionX = 0;} 
-        else {this.startPostitionX = lvlSizeX;}
+    public int setStartPositionX() {
+        int temp = (int)Math.round(Math.random()*Game.getLimitX());
+        int startPositionX;
+        if(temp < Game.getLimitX()/2){startPositionX = 0;} 
+        else {startPositionX = Game.getLimitX();}
+        
+        return startPositionX;
     }
 
-    public void setStartPostitionY(int lvlSizeY) {
-        int temp = (int)Math.round(Math.random()*(lvlSizeY-1));
-        this.startPostitionY = temp;
+    public int setStartPositionY() {
+        int startPositionY = (int)Math.round(Math.random()*(Game.getLimitY()-2))+2;  
+        
+        return startPositionY;
     }
     
     
