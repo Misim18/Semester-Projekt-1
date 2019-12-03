9b3122320a9d153c3da0517cc37b1cfa23136781
Michael-Laptop <Michael-Laptop@DESKTOP-01AS2DF>
2019-12-03 13:55:03 +0100

    Added new cheat goToShop, changed functionality of getAllItem cheat. Added HashMap in character to hold information about types of collectables. Added test function to print the contents of said HashMap Added functionality so types of collectables are now checked and stored in said HashMap (fixed version)

--- World of Zuul/src/main/java/com/group4/gui/App.java
+++ World of Zuul/src/main/java/com/group4/gui/App.java
@@ -1,10 +1,18 @@
 package com.group4.gui;
 
+import com.group4.gameLogic.Command;
+import com.group4.gameLogic.CommandWord;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 import java.io.IOException;
+
+import com.group4.gameLogic.Game;
+
 import javafx.stage.Stage;
 
 /**
@@ -12,12 +20,22 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    static Game game;
     private static Scene scene;
+
+    public static void injectGame(Game game1) {
+        game = game1;
+    }
+
+    public static Game getGame() {
+        return game;
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 1280, 800);
         stage.setScene(scene);
+        addKeyEventScene();
         stage.show();
     }
 
@@ -30,9 +48,29 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void load(String[] args) {
+    public static void startFX(String[] args) {
         launch(args);
     }
 
+    public void addKeyEventScene() {
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if (key.getCode() == KeyCode.S) {
+                System.out.println("You pressed down");
+                App.game.goRoom(new Command(CommandWord.GO, "down"));
+            }
+            if (key.getCode() == KeyCode.W) {
+                System.out.println("You pressed up");
+                App.game.goRoom(new Command(CommandWord.GO, "up"));
+            }
+            if (key.getCode() == KeyCode.A) {
+                System.out.println("You pressed left");
+                App.game.goRoom(new Command(CommandWord.GO, "left"));
+            }
+            if (key.getCode() == KeyCode.D) {
+                System.out.println("You pressed right");
+                App.game.goRoom(new Command(CommandWord.GO, "right"));
+            }
+        });
+    }
 
 }
