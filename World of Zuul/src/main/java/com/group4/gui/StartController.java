4efd4309d98293b5b5b85068cd4a43afbfb32202
kepet19 <kevinmeejach@gmail.com>
2019-12-04 19:56:35 +0100

    Add so it can load name now. if the box is empty nothing happens.

--- World of Zuul/src/main/java/com/group4/gui/StartController.java
+++ World of Zuul/src/main/java/com/group4/gui/StartController.java
@@ -5,8 +5,10 @@ import com.group4.gameLogic.CommandWord;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class StartController {
 
@@ -33,18 +35,32 @@ public class StartController {
 
     @FXML
     void handleScoreboardButtonAction(ActionEvent event) {
+		showAlertWithNotImplemented();
     }
 
     @FXML
     void handleLoadButtonAction(ActionEvent event) {
 		App.game.processCommand(new Command(CommandWord.LOAD, null));
-		App.toggleUI();
+		textFieldPlayerName.setText(App.game.player1.getName());
     }
 
     @FXML
     void handlePlayButtonAction(ActionEvent event) {
-		App.game.player1.setName("Temp Name");
+		if(textFieldPlayerName.equals("")){
+			App.game.player1.setName("You Did Not Put In A Name");
+		} else {
+			App.game.player1.setName(textFieldPlayerName.getText());
+		}
 		App.toggleUI();
     }
 
+	    // Show a Information Alert with default header Text
+    private void showAlertWithNotImplemented() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("This is not implemented: yet");
+
+        // alert.setHeaderText("Results:");
+        alert.setContentText("This is not implemented: yet");
+        alert.showAndWait();
+    }
 }
