c6c0c1060791f7c68568d1b3138842d27f74e3e6
miki2670 <mikailcengiz@live.dk>
2019-12-09 13:56:23 +0100

    Added death and intro and their controllers and some methods in them

---
+++ World of Zuul/src/main/java/com/group4/gui/DeathController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.group4.gui;
+
+import com.group4.gameLogic.Text;
+import com.group4.gameLogic.Character;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+/**
+ *
+ * @author mikai
+ */
+public class DeathController {
+    
+    @FXML // fx:id="levelReachedText"
+    private TextArea levelReachedText;
+    
+    @FXML
+    private Button bBackToStart;
+
+    @FXML
+    public void initialize() {
+        levelReachedText.setText("level reached here"); 
+    }
+    
+    @FXML
+    void handlehandleBackToStartButtonAction(ActionEvent event) {
+		App.toggleUI();
+    }
+    
+}
---
+++ World of Zuul/src/main/java/com/group4/gui/IntroController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.group4.gui;
+
+import com.group4.gameLogic.Text;
+import com.group4.gameLogic.Character;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+/**
+ *
+ * @author mikai
+ */
+public class IntroController {
+    
+    @FXML // fx:id="textFieldIntro"
+    private TextArea textFieldIntro;
+    
+    @FXML
+    private Button bIUnderstandIntro;
+
+    @FXML
+    public void initialize() {
+        textFieldIntro.setText("Hej dette er intro teksten"); //PrintWelcome method should be here
+    }
+    
+    @FXML
+    void handleIUnderstandIntroButtonAction(ActionEvent event) {
+		App.toggleUI();
+    }
+    
+}
