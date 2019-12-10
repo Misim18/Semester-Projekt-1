3e38898c98a8376351bbef43ce6b448655c5730b
chmad18 <chmad18@student.sdu.dk>
2019-12-10 12:25:40 +0100

    Added textbubble for captain

--- World of Zuul/src/main/java/com/group4/gui/ShopController.java
+++ World of Zuul/src/main/java/com/group4/gui/ShopController.java
@@ -1,54 +1,67 @@
 package com.group4.gui;
 
-
 import com.group4.gameLogic.CommandShop;
 import com.group4.gameLogic.CommandWordShop;
 import com.group4.gameLogic.Text;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javax.imageio.ImageIO;
 
 public class ShopController {
-    @FXML // fx:id="bBuyInventoryUpgrade"
-    private Button bBuyInventoryUpgrade; // Value injected by FXMLLoader
 
-    @FXML // fx:id="bBuyBreathUpgrade"
-    private Button bBuyBreathUpgrade; // Value injected by FXMLLoader
+    @FXML
+    private Button bBuyInventoryUpgrade;
 
-    @FXML // fx:id="bNextLevel"
-    private Button bNextLevel; // Value injected by FXMLLoader
+    @FXML
+    private Button bBuyBreathUpgrade;
 
-    @FXML // fx:id="textFieldPirate"
-    private TextArea textFieldPirate; // Value injected by FXMLLoader
+    @FXML
+    private Button bNextLevel;
+
+    @FXML
+    private ImageView bubble;
+    
+    
+    @FXML
+    private TextArea textbubble;
 
     @FXML
     void handleBuyBreathUpgradeButtonAction(ActionEvent event) {
-		App.game.shop.processShopCommand(new CommandShop(CommandWordShop.BREATH, null));
-		uiUpdate();
+        App.game.shop.processShopCommand(new CommandShop(CommandWordShop.BREATH, null));
+        uiUpdate();
     }
 
     @FXML
     void handleInventoryUpgradeButtonAction(ActionEvent event) {
-		App.game.shop.processShopCommand(new CommandShop(CommandWordShop.INVENTORY, null));
-		uiUpdate();
+        App.game.shop.processShopCommand(new CommandShop(CommandWordShop.INVENTORY, null));
+        uiUpdate();
     }
 
     @FXML
     void handleNextLevelButtonAction(ActionEvent event) {
-		App.game.shop.processShopCommand(new CommandShop(CommandWordShop.NEXTLEVEL, null));
-		App.toggleUI();
+        App.game.shop.processShopCommand(new CommandShop(CommandWordShop.NEXTLEVEL, null));
+        App.toggleUI();
     }
 
-	@FXML
-    public void initialize() {
-		textFieldPirate.setText(Text.getRandLine()); //FILL HERE
-		uiUpdate();
-	}
+    @FXML
+    public void initialize() throws Exception {
+        textbubble.setText(Text.getRandLine());
 
-	public void uiUpdate(){
+        uiUpdate();
 
-	}
+    }
+
+    public void uiUpdate() {
+
+    }
 }
-
